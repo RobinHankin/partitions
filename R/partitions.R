0a6e8ff0e3a54439c3f005c24f05e97584da4f66
pegeler <32426645+pegeler@users.noreply.github.com>
2020-09-22 22:44:41 -0400

    Issue 7 (#14)
    * conjugate and durfee take data in non-standard form
    
    * sorting data in conjugate using simple counting sort
    
    * closes #9
    
    * updating docs
    
    * added tests for durfee and conjugate, closing #11
    
    * in-place sorting
    
    * fixing order of operands in calloc
    
    * adding appveyor CI
    
    * fixing MacOS note about mathjaxr
    
    * fixing winbuilder warning regarding calloc
    
    * updating tests to work with testthat
    
    * closes #7
    
    * adding a test for issue #7
    
    * cleaner solution for #7
    
    * updating travis

--- R/partitions.R
+++ R/partitions.R
@@ -63,8 +63,10 @@ print.summary.partition <- function(x, ...){
     return(Recall(parts(x)))
   }
   if(is.matrix(x)){
-    out <- apply(x,2,setparts)
-    if(is.list(out)){out <- do.call("cbind",out)}
+    out <- do.call(
+      "cbind",
+      lapply(seq_len(ncol(x)), function(i) setparts(x[, i]))
+    )
   } else {
     x <- sort(x[x>0], decreasing=TRUE)
     num.of.parts <-
@@ -77,7 +79,7 @@ print.summary.partition <- function(x, ...){
               )$ans
     dim(out) <- c(sum(x),num.of.parts)
   }
-  return(as.partition(out))
+  as.partition(out)
 }
 
 "listParts" <- function(x,do.set=FALSE) {
