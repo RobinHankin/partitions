3928f47ee4dfa2fe1c08326dda1c0dc60373172b
Robin Hankin <hankin.robin@gmail.com>
2021-10-01 20:17:49 +1300

    more accuracy

--- inst/partitions_stickermaker.R
+++ inst/partitions_stickermaker.R
@@ -13,5 +13,5 @@ points(rbind(
 dev.off()
 
 sticker("partitions_icon.bmp", package="partitions", p_size=28, s_x=1, s_y=0.64,
-s_width=0.9,asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
+s_width=0.9,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
 h_color="#000000", filename="partitions.png")
